3d67dcd3436cb60d798aeeab72fdd9d8484e7d54
Kim, Sinhyuk <rlatls1997@gmail.com>
2020-05-06 10:00:49 +0900

    10:00

--- public/midterm/mid5.js
+++ public/midterm/mid5.js
@@ -1,9 +1,10 @@
 let a = [ "hello.html", "world.js", "readme.txt" ];
 
 function getExtension(fileName){
-
+    let start = fileName.indexOf(".")
+    console.log(fileName.substring(start,fileName.length))
 }
 
 for(let i = 0; i<a.length; i++){
-    
+    getExtension(a[i])
 }
